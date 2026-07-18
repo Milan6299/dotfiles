8d304c6ea925f62dc29119ee475e4f110736e014
Milan6299 <milanmahapatra140@gmail.com>
2026-07-18 10:58:54 +0530

    disabled dadbod query notification

--- nvim/lua/plugins/dadbod.lua
+++ nvim/lua/plugins/dadbod.lua
@@ -13,5 +13,7 @@ return {
   init = function()
     -- Your DBUI configuration
     vim.g.db_ui_use_nerd_fonts = 1
+    -- vim.g.db_ui_disable_progress_bar = 1
+    vim.g.db_ui_disable_info_notifications = 1
   end,
 }
